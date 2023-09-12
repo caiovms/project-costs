dce5a8cb4fb2a5580337ab6376e8865cbac8c397
Caio Silva <caio_vms@outlook.com>
2023-09-12 16:26:38 -0300

    #Class18: Structuring project.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Cointainer'
+
 function App() {
   return (
-    <div className="App">
-        <p>Costs</p>
-    </div>
-  );
+    <Route>
+      <ul>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/company'>Company</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/newproject'>NewProject</Link>
+        </div>
+        <Switch>
+          <Container customClass='min-height'>
+            <Route exact path='/'>
+              <Home/>
+            </Route>
+            <Route exact path='/company'>
+            </Route>
+              <Company />
+            <Route exact path='/contact'>
+            </Route>
+              <Contact />
+            <Route exact path='/newproject'>
+              <NewProject />
+            </Route>
+          </Container>
+        </Switch>
+      </ul>
+      <p>Footer</p>
+    </Route>
+  )
 }
 
-export default App;
+export default App
