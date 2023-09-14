14bc195e4fd23929de650458a9f80f61569cbd3e
Caio Silva <caio_vms@outlook.com>
2023-09-14 16:06:40 -0300

    #Class19: Navbar and Footer.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+
 import Home from './components/pages/Home'
+import Projects from './components/pages/Project'
+import NewProject from './components/pages/NewProject'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
-import NewProject from './components/pages/NewProject'
-
-import Container from './components/layout/Cointainer'
 
 function App() {
   return (
-    <Route>
-      <ul>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/company'>Company</Link>
-          <Link to='/contact'>Contact</Link>
-          <Link to='/newproject'>NewProject</Link>
-        </div>
-        <Switch>
-          <Container customClass='min-height'>
-            <Route exact path='/'>
-              <Home/>
-            </Route>
-            <Route exact path='/company'>
-            </Route>
-              <Company />
-            <Route exact path='/contact'>
-            </Route>
-              <Contact />
-            <Route exact path='/newproject'>
-              <NewProject />
-            </Route>
-          </Container>
-        </Switch>
-      </ul>
-      <p>Footer</p>
-    </Route>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
 
