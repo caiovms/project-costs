4f3b7a5353680547060e2d9e58e5be3c3ba47598
Caio Silva <caio_vms@outlook.com>
2023-09-18 11:10:40 -0300

    #Class20: Home.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,17 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start management your projects right now!</p>
+            <LinkButton to='/newproject' text='Create Project'></LinkButton>
+            <img src={savings} alt='Costs'></img>
+        </section>
+    )
 }
 
 export default Home
