f6ff144c33184a17e3ac7b701d78cccaee1bdc8b
Caio Silva <caio_vms@outlook.com>
2023-09-19 11:51:03 -0300

    #Class23: Connecting with fake API.

--- src/components/from/Select.js
+++ src/components/from/Select.js
@@ -1,14 +1,24 @@
 import styles from './Select.module.css'
 
-function Select({text, name, options, hanleOnChange, value}) {
-    return (
-        <div className={styles.form.control}>
-            <label htmlFor={name}>{text}</label>
-            <select name={name} id={name}>
-                <options>Select an option</options>
-            </select>
-        </div>
-    )
+function Select({ text, name, options, handleOnChange, value }) {
+  return (
+    <div className={styles.form_control}>
+      <label htmlFor={name}>{text}:</label>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ''}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((options) => (
+          <option value={options.id} key={options.id}>
+            {options.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
 }
 
 export default Select
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../from/Input'
 import Select from '../from/Select'
 import SubmitButton from '../from/SubmitButton'
@@ -5,29 +7,44 @@ import SubmitButton from '../from/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setCategories(data)
+          })
+      }, [])
+
     return (
         <form className={styles.form}>
-            <Input 
-                type='text' 
-                text='Project Name' 
-                name='name' 
-                placeholder='Insert project name'
-            />
-            <Input 
-                type='number' 
-                text='Project budget' 
-                name='budget' 
-                placeholder='Insert total budget'
-            />
-            <Select 
-                name='category_id' 
-                text='Select category'
-            />
-            <SubmitButton 
-                text={btnText}
-            />
+          <Input
+            type="text"
+            text="Project Name"
+            name="name"
+            placeholder="Insert project name"
+          />
+          <Input
+            type="number"
+            text="Project budget"
+            name="budget"
+            placeholder="Insert total budget"
+          />
+          <Select
+            name="category_id"
+            text="Select category"
+            options={categories}
+          />
+          <SubmitButton text={btnText} />
         </form>
-    )
-}
+      )
+    }
 
 export default ProjectForm
