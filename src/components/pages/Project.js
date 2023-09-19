5856c7139730056bfa80f77730b6cd3a5d405b82
Caio Silva <caio_vms@outlook.com>
2023-09-19 19:57:27 -0300

    #Class26: Creating dashboard

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,6 +1,10 @@
 import { useLocation } from 'react-router-dom'
 
+import Container from '../layout/Container'
 import Message from "../layout/Message"
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Project.module.css'
 
 function Project() {
 
@@ -13,9 +17,15 @@ function Project() {
     }
 
     return (
-        <div>
-            <h1>My Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My Projects</h1>
+                <LinkButton to='/newproject' text='Create Project'></LinkButton>
+            </div>
             {message && <Message msg={message} type='success'/>}
+            <Container customClass="start">
+                <p>Projects...</p>
+            </Container>
         </div>
     )
 }
