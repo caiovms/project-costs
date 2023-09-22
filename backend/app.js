0cb1bdd1798398c7ca6ed69a35b2b2fbae864c20
caiovms <caio_vms@outlook.com>
2023-09-22 08:59:10 -0300

    Create backend initial structure

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(8000, function(){
+    console.log("Server online!")
+})
