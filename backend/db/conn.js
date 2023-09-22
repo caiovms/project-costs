7f913676c7014ffc006cc158267f7c161b95cc38
caiovms <caio_vms@outlook.com>
2023-09-22 15:48:51 -0300

    Add docker-compose file

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,7 +4,8 @@ async function main(){
 
     try
     {
-        await mongoose.connect("mongodb+srv://admin:mXd2lPgdYhCtgxtR@cluster0.eqvsbch.mongodb.net/?retryWrites=true&w=majority")
+        // await mongoose.connect("mongodb+srv://admin:mXd2lPgdYhCtgxtR@cluster0.eqvsbch.mongodb.net/?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb://admin:mXd2lPgdYhCtgxtR@localhost:27017");
         
         console.log("[Project-Cost] - Database connection successfully.")
     } 
